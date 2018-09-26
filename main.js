e5956ce356207a8cfefc73de73c180b474eeefb1
devlol123 <43362053+devlol123@users.noreply.github.com>
2018-09-26 22:22:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function show() {
+
+
+var el = document.querySelector('html');
+              el.style.overflow = 'visible';
+
+var el2 = document.querySelector('body');
+              el2.style.overflow = 'visible';
+}
